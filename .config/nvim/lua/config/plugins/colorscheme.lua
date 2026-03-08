78899a109e7c20ff91d46c8b35c019e9edbc8112
bvsvntv <developer.basanta@gmail.com>
2026-03-08 12:24:27 +0545

    Use gruber darker theme

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,16 +1,13 @@
-local function apply_color_scheme(colorscheme)
-	vim.cmd.colorscheme(colorscheme)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
+		"blazkowolf/gruber-darker.nvim",
+		opts = {
+			italic = {
+				comments = true,
+			},
+		},
 		config = function()
-			apply_color_scheme("catppuccin-macchiato")
+			vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
 }

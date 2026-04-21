ae71f1432446bb49c067087b36d34129bbefb1d9
bvsvntv <developer.basanta@gmail.com>
2026-04-21 21:17:01 +0545

    Move lsp related stuff into dedicated directory

--- .config/nvim/lua/bvsvntv/pack.lua
+++ .config/nvim/lua/bvsvntv/pack.lua
@@ -21,11 +21,12 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" },
 })
 
+require("bvsvntv.plugins.lsp.mason")
+require("bvsvntv.plugins.lsp.lsp_config")
+
 require("bvsvntv.plugins.conform")
 require("bvsvntv.plugins.gitsigns")
 require("bvsvntv.plugins.gruber_darker")
-require("bvsvntv.plugins.lsp")
-require("bvsvntv.plugins.mason")
 require("bvsvntv.plugins.mini")
 require("bvsvntv.plugins.telescope")
 require("bvsvntv.plugins.treesitter")

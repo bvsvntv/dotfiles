cafda5da87e00d6c8f2583bee65b3241ced47a62
bvsvntv <developer.basanta@gmail.com>
2026-04-26 11:51:27 +0545

    Add keymaps to perform hunk level operations

--- .config/nvim/lua/bvsvntv/plugins/gitsigns.lua
+++ .config/nvim/lua/bvsvntv/plugins/gitsigns.lua
@@ -1,1 +1,8 @@
 require("gitsigns")
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<Leader>gp", ":Gitsigns preview_hunk<CR>", opts)
+vim.keymap.set("n", "<Leader>ghs", ":Gitsigns stage_hunk<CR>", opts)
+vim.keymap.set("n", "<Leader>ghr", ":Gitsigns reset_hunk<CR>", opts)
+vim.keymap.set("n", "]h", ":Gitsigns next_hunk<CR>", opts)
+vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<CR>", opts)

39521cf02c86b1af481e1de8ee42a8ba67bf0b09
bvsvntv <developer.basanta@gmail.com>
2026-04-26 11:52:35 +0545

    Make use of combination with capital letters for merge conflict operations

--- .config/nvim/lua/bvsvntv/plugins/vim_fugitive.lua
+++ .config/nvim/lua/bvsvntv/plugins/vim_fugitive.lua
@@ -2,7 +2,7 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<Leader>gs", vim.cmd.Git, opts)
 vim.keymap.set("n", "<Leader>mc", ":Gvdiffsplit!<CR>", opts)
-vim.keymap.set("n", "<Leader>gl", ":diffget //3<CR>", opts)
-vim.keymap.set("n", "<Leader>gh", ":diffget //2<CR>", opts)
+vim.keymap.set("n", "<Leader>gL", ":diffget //3<CR>", opts)
+vim.keymap.set("n", "<Leader>gH", ":diffget //2<CR>", opts)
 vim.keymap.set("n", "<Leader>gu", ":diffupdate<CR>", opts)
 vim.keymap.set("n", "<Leader>bw", ":Git blame<CR>", opts)

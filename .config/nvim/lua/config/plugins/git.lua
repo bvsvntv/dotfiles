27d3575a5e9bef4e573a7bd4d526c4459e5a6586
bvsvntv <developer.basanta@gmail.com>
2026-05-06 13:48:52 +0545

    Add keymap to stage/unstage and navigate between hunks

--- .config/nvim/lua/config/plugins/git.lua
+++ .config/nvim/lua/config/plugins/git.lua
@@ -6,8 +6,8 @@ return {
 		config = function()
 			vim.keymap.set("n", "<Leader>gs", vim.cmd.Git, opts)
 			vim.keymap.set("n", "<Leader>mc", ":Gvdiffsplit!<CR>", opts)
-			vim.keymap.set("n", "<Leader>gl", ":diffget //3<CR>", opts)
-			vim.keymap.set("n", "<Leader>gh", ":diffget //2<CR>", opts)
+			vim.keymap.set("n", "<Leader>gL", ":diffget //3<CR>", opts)
+			vim.keymap.set("n", "<Leader>gH", ":diffget //2<CR>", opts)
 			vim.keymap.set("n", "<Leader>gu", ":diffupdate<CR>", opts)
 			vim.keymap.set("n", "<Leader>bw", ":Git blame<CR>", opts)
 		end,
@@ -18,7 +18,16 @@ return {
 		config = function()
 			require("gitsigns").setup({})
 
-			vim.keymap.set("n", "<Leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set(
+				"n",
+				"<Leader>gp",
+				":Gitsigns preview_hunk<CR>",
+				opts
+			)
+			vim.keymap.set("n", "<Leader>ghs", ":Gitsigns stage_hunk<CR>", opts)
+			vim.keymap.set("n", "<Leader>ghr", ":Gitsigns reset_hunk<CR>", opts)
+			vim.keymap.set("n", "<Leader>nh", ":Gitsigns next_hunk<CR>", opts)
+			vim.keymap.set("n", "<Leader>Nh", ":Gitsigns prev_hunk<CR>", opts)
 		end,
 	},
 }
